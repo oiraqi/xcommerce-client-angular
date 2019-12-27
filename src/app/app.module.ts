809b6aae80c69945d76db1cfb00bc91a239817d6
Omar Iraqi <o.iraqi@gmail.com>
2019-12-27 15:57:01 +0100

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CoreModule } from './core/core.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoreModule } from './core/core.module';
 import { CatalogModule } from './catalog/catalog.module';
 
 @NgModule({
--- src/app/catalog/catalog.module.ts
+++ src/app/catalog/catalog.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from './components/products/product/product.component';
-import { MatCardModule,
-         MatListModule,
-         MatGridListModule,
-         MatIconModule,
-         MatDividerModule,
-         MatMenuModule } from '@angular/material';
+import {
+  MatCardModule,
+  MatButtonModule,
+  MatListModule,
+  MatGridListModule,
+  MatIconModule,
+  MatDividerModule,
+  MatMenuModule
+} from '@angular/material';
 import { ProductsComponent } from './components/products/products.component';
 import { BrandsComponent } from './components/brands/brands.component';
 import { CategoriesComponent } from './components/categories/categories.component';
@@ -15,13 +18,14 @@ import { CategoriesComponent } from './components/categories/categories.componen
   declarations: [ProductComponent, ProductsComponent, BrandsComponent, CategoriesComponent],
   imports: [
     CommonModule,
+    MatMenuModule,
+    MatButtonModule,
     MatCardModule,
     MatListModule,
     MatGridListModule,
     MatIconModule,
-    MatDividerModule,
-    MatMenuModule
+    MatDividerModule
   ],
-  exports: [ProductComponent, ProductsComponent]
+  exports: [ProductsComponent, BrandsComponent, CategoriesComponent]
 })
 export class CatalogModule { }
